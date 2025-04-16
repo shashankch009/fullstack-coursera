c32564e72ec1a533ed63dbde2c6e0be966a60230
Shashank Choudhary <shashank.ch009@gmail.com>
2025-04-16 15:32:05 -0600

    enabling controllers in program.cs

--- Backend/WebApi-M2/Program.cs
+++ Backend/WebApi-M2/Program.cs
@@ -1,6 +1,10 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers(); // Added to register controllers
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+app.MapControllers(); // Added to map controller routes
 
 app.Run();
