383d937f7bacdd23da1480b88bf1c681bec1d0d7
Shashank Choudhary <shashank.ch009@gmail.com>
2025-04-16 22:24:38 -0600

    adding simple middleware

--- Backend/WebApi-M2/Program.cs
+++ Backend/WebApi-M2/Program.cs
@@ -9,6 +9,17 @@ builder.Services.AddSingleton<ProductService>();
 
 var app = builder.Build();
 
+app.Use(async (context, next) =>
+{
+    // context is of type Microsoft.AspNetCore.Http.HttpContext
+    // next is of type System.Func<Microsoft.AspNetCore.Http.HttpContext, System.Threading.Tasks.Task>
+    // Do work that can write to the Response.
+    Console.WriteLine("logic before");
+    await next.Invoke();
+    // Do logging or other work that doesn't write to the Response.
+    Console.WriteLine("logic after");
+});
+
 app.MapGet("/", () => "Hello World!");
 
 app.MapControllers(); // Added to map controller routes
