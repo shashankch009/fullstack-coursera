032898fb0d0fd0485e2db308915586ce89411a5c
Shashank Choudhary <shashank.ch009@gmail.com>
2025-04-16 22:26:35 -0600

    adding missed part of second last commit

---
+++ Backend/WebApi-M2/Models/Product.cs
@@ -0,0 +1,10 @@
+
+namespace WebApi_M2.Models;
+
+public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public decimal Price { get; set; }
+}
