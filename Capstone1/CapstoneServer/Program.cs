b281a6227279d982a4c17135f92933662317602a
Shashank Choudhary <shashank.ch009@gmail.com>
2025-04-20 21:49:29 -0600

    adding basic products api in server and calling it from client app

--- Capstone1/CapstoneServer/Program.cs
+++ Capstone1/CapstoneServer/Program.cs
@@ -1,6 +1,20 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(builder =>
+    {
+        builder.AllowAnyHeader().AllowAnyMethod().AllowCredentials().SetIsOriginAllowed(_ => true);
+    });
+});
+
 var app = builder.Build();
 
+app.UseCors();
+
 app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/api/products", () =>
+    new[] { new { Id = 1, Name = "Laptop" }, new { Id = 2, Name = "Phone" } });
 
 app.Run();
