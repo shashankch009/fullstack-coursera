e6e937740026f7fd397c581a0cb42a403c298ebe
Shashank Choudhary <shashank.ch009@gmail.com>
2025-04-23 17:24:06 -0600

    requirign authorisation on some requests and returning 401 in such case

--- SecurityCourse/ApiWithAuth/Program.cs
+++ SecurityCourse/ApiWithAuth/Program.cs
@@ -13,15 +13,40 @@ builder.Services.AddIdentity<IdentityUser, IdentityRole>()
 builder.Services.AddAuthentication();
 builder.Services.AddAuthorizationBuilder();
 
+
+//this will ensure that if user is not authenticated, calls to /api routes will return 401 status code instead of redirecting to login page
+builder.Services.ConfigureApplicationCookie(options => {
+    options.Events.OnRedirectToLogin = context => {
+        Console.WriteLine("Redirecting to login page...");
+        if(context.Response.StatusCode == StatusCodes.Status200OK) {
+            context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+            return Task.CompletedTask;
+        }
+        else {
+            context.Response.Redirect(context.RedirectUri);
+            return Task.CompletedTask;
+        }
+    };
+});
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
-app.MapGet("/admin", () => "Admin only route");
+app.MapGet("/account/admin", () => "Admin only route")
+    .RequireAuthorization();
 
-app.MapGet("/user", () => "User only route");  
+app.MapGet("/account/user", () => "User only route")
+    .RequireAuthorization();  
 
-app.MapGet("user-it", () => "User claim route for IT department");
+app.MapGet("/account/user-it", () => "User claim route for IT department")
+    .RequireAuthorization();
+
+//without this call, if authorisation fails for above routes 
+//which require authorisation, above calls will return 404 not found, which is not valid response
+//that happens beacuse when authorisation fails, calls are redirected to /account/login page
+//so we need to add this api route. 
+app.MapGet("/account/login", () => "Login route");
 
 app.MapGet("/public", () => "Public route");
 
