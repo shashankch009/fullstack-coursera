34b8ad4903379a5d617d6d8637072133684f3f6f
Shashank Choudhary <shashank.ch009@gmail.com>
2025-04-23 16:43:36 -0600

    adding basic api calls for testing - ApiWithAuth

--- SecurityCourse/ApiWithAuth/Program.cs
+++ SecurityCourse/ApiWithAuth/Program.cs
@@ -3,4 +3,13 @@ var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
+app.MapGet("/admin", () => "Admin only route");
+
+app.MapGet("/user", () => "User only route");  
+
+app.MapGet("user-it", () => "User claim route for IT department");
+
+app.MapGet("/public", () => "Public route");
+
+
 app.Run();
