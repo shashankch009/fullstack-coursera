31756e9d86d18b4eec0eb93a502c4475c1916f94
Shashank Choudhary <shashank.ch009@gmail.com>
2025-04-23 23:27:23 -0600

    trying roles, claims

--- SecurityCourse/ApiWithAuth/Program.cs
+++ SecurityCourse/ApiWithAuth/Program.cs
@@ -1,3 +1,4 @@
+using System.Security.Claims;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
@@ -11,7 +12,10 @@ builder.Services.AddIdentity<IdentityUser, IdentityRole>()
     .AddEntityFrameworkStores<IdentityDbContext>();
 
 builder.Services.AddAuthentication();
-builder.Services.AddAuthorizationBuilder();
+builder.Services.AddAuthorizationBuilder()
+    .AddPolicy("AdminOnly", policy => policy.RequireRole("Admin"))
+    .AddPolicy("ITDepartmentOnly", policy => policy.RequireClaim("Department", "IT"));
+
 
 
 //this will ensure that if user is not authenticated, calls to /api routes will return 401 status code instead of redirecting to login page
@@ -31,16 +35,18 @@ builder.Services.ConfigureApplicationCookie(options => {
 
 var app = builder.Build();
 
+string [] roles = new string[] { "Admin", "User"};
+
 app.MapGet("/", () => "Hello World!");
 
 app.MapGet("/account/admin", () => "Admin only route")
-    .RequireAuthorization();
+    .RequireAuthorization("AdminOnly");
 
 app.MapGet("/account/user", () => "User only route")
     .RequireAuthorization();  
 
 app.MapGet("/account/user-it", () => "User claim route for IT department")
-    .RequireAuthorization();
+    .RequireAuthorization("ITDepartmentOnly");
 
 //without this call, if authorisation fails for above routes 
 //which require authorisation, above calls will return 404 not found, which is not valid response
@@ -50,5 +56,92 @@ app.MapGet("/account/login", () => "Login route");
 
 app.MapGet("/public", () => "Public route");
 
+app.MapPost("/account/roles", async (RoleManager<IdentityRole> RoleManager) => {
+
+    foreach(var role in roles) {
+        if(!await RoleManager.RoleExistsAsync(role)) {
+            await RoleManager.CreateAsync(new IdentityRole(role));
+        }
+    }
+    return Results.Ok("Roles created successfully");
+});
+
+app.MapPost("/account/user/{id}/assign-role/{role}", async (int id, string role, UserManager<IdentityUser> UserManager) => {
+    var user = await UserManager.FindByIdAsync(id.ToString());
+    if(user == null) {
+        return Results.NotFound("User not found");
+    }
+    var result = await UserManager.AddToRoleAsync(user, role);
+    if(result.Succeeded) {
+        return Results.Ok("Role assigned successfully");
+    }
+    return Results.BadRequest("Failed to assign role");
+});
+
+//below ones for testing only, to create user and assign roles
+
+app.MapPost("/account/register-test", async (UserManager<IdentityUser> userManager)  => 
+{
+    var testUser = new IdentityUser { UserName = "testuser", Email = "testuser@gmail.com"};
+    var result = await userManager.CreateAsync(testUser, "Test@123");
+    if(result != null && result.Succeeded) 
+    {
+        Console.WriteLine($"User created successfully. ID : {testUser.Id}");
+        return Results.Ok($"User created successfully with Id: {testUser.Id}");
+    }
+    else 
+    {
+        return Results.BadRequest("Failed to create user");
+    }
+});
+
+app.MapPost("/account/assign-admin-test", async (UserManager<IdentityUser> userManager) => {
+    var user = await userManager.FindByEmailAsync("testuser@gmail.com");
+    if(user == null) 
+    {
+        return Results.NotFound("User not found");
+    }
+    var result = await userManager.AddToRoleAsync(user, "Admin");
+    if(result.Succeeded) 
+    {
+        return Results.Ok("User assigned to Admin role successfully");
+    }
+    else 
+    {
+        return Results.BadRequest("Failed to assign user to Admin role");
+    }
+});
+
+app.MapPost("/account/add-claim-test", async (UserManager<IdentityUser> userManager) => 
+{
+    var user = await userManager.FindByEmailAsync("testuser@gmail.com");
+    if(user == null) 
+    {
+        return Results.NotFound("User not found");
+    }
+    var claim = new Claim("Department", "IT"); //key, value pair
+    var result = await userManager.AddClaimAsync(user, claim);
+    if(result.Succeeded) 
+    {
+        return Results.Ok("Claim added successfully");
+    }
+    else 
+    {
+        return Results.BadRequest("Failed to add claim to user");
+    }
+});
+
+app.MapPost("/account/login-test", async (SignInManager<IdentityUser> signInManager) => 
+{
+    var result = await signInManager.PasswordSignInAsync("testuser", "Test@123", isPersistent: false, lockoutOnFailure: false);
+    if(result.Succeeded) 
+    {
+        return Results.Ok("User logged in successfully");
+    }
+    else 
+    {
+        return Results.BadRequest("Failed to login user");
+    }
+});
 
 app.Run();
